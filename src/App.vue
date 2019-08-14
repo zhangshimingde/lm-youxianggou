9b02dfe67636f75a8a8844dbd37aae39c57b6df6
Unknown <unknown@example.com>
2019-08-14 23:04:41 +0800

    vue + ts 项目搭建完成
    vue + ts 项目搭建完成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <keep-alive>
+            <router-view v-if="$route.meta.keepAlive"/>
+        </keep-alive>
+        <router-view v-if="!$route.meta.keepAlive"/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
